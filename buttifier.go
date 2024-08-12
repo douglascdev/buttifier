4c946f623ae14ba422443e585cd31fb9cb3f52db
douglascdev <douglasc.dev@gmail.com>
2024-08-12 13:59:18 -0300

    Buttify sentences

--- buttifier.go
+++ buttifier.go
@@ -67,22 +67,47 @@ func newHyphenator(hyphenationFile string) (*hyphenation.Lang, error) {
 	return hyphenation.New(r)
 }
 
-// replace random syllables with buttWord and return the result
-func (b *Buttifier) Buttify(word string) string {
+// replace random syllables with buttWord
+// returns the buttified word and the number of buttified syllables
+func (b *Buttifier) ButtifyWord(word string) (string, int) {
 	breakpoints := b.hyphenator.Hyphenate(word)
 	// Hyphenate doesn't return the last syllable as a breakpoint so we add it
 	breakpoints = append(breakpoints, len(word))
 	var wordBuffer strings.Builder
 	prev := 0
+	buttCount := 0
 	for _, breakPoint := range breakpoints {
 		rn := rand.New(b.RandSource).Float64()
 		if rn < b.buttificationRate {
 			wordBuffer.WriteString(b.buttWord)
+			buttCount++
 		} else {
 			wordBuffer.WriteString(word[prev:breakPoint])
 		}
 		prev = breakPoint
 	}
 
-	return wordBuffer.String()
+	return wordBuffer.String(), buttCount
+}
+
+// replace random syllables from each word with buttWord
+// returns the buttified word and true if the word was changed
+func (b *Buttifier) ButtifySentence(sentence string) (string, bool) {
+	rn := rand.New(b.RandSource).Float64()
+	toButtOrNotToButt := rn < b.buttificationProbability
+
+	if !toButtOrNotToButt {
+		return sentence, false
+	}
+
+	words := strings.Split(sentence, " ")
+	changed := false
+	for i := range words {
+		buttifiedWord, count := b.ButtifyWord(words[i])
+		if count > 0 {
+			words[i] = buttifiedWord
+			changed = true
+		}
+	}
+	return strings.Join(words, " "), changed
 }
--- buttifier_test.go
+++ buttifier_test.go
@@ -11,7 +11,7 @@ func (UnitTestRandSource) Uint64() uint64 {
 	return 0
 }
 
-func TestButtify(t *testing.T) {
+func TestButtifyWord(t *testing.T) {
 	b, err := buttifier.New()
 	b.RandSource = UnitTestRandSource{}
 	if err != nil {
@@ -22,7 +22,24 @@ func TestButtify(t *testing.T) {
 		"someone":    "buttbutt",
 	}
 	for word, expected := range resultMap {
-		actual := b.Buttify(word)
+		actual, _ := b.ButtifyWord(word)
+		if expected != actual {
+			t.Errorf("expected %s, got %s", expected, actual)
+		}
+	}
+}
+
+func TestButtifySentence(t *testing.T) {
+	b, err := buttifier.New()
+	b.RandSource = UnitTestRandSource{}
+	if err != nil {
+		t.Fatal(err)
+	}
+	resultMap := map[string]string{
+		"grinding for partner": "buttbutt butt buttbutt",
+	}
+	for sentence, expected := range resultMap {
+		actual, _ := b.ButtifySentence(sentence)
 		if expected != actual {
 			t.Errorf("expected %s, got %s", expected, actual)
 		}
